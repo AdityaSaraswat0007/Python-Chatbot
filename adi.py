b82b5924e6cf90b0564351ce5178bbe7a6567da3
Kunal Srivastav <sri2001kunal@gmail.com>
2020-09-30 19:16:44 +0530

    change the `r` to `result`
    use of proper naming conventions makes program better and easy to understand.

--- adi.py
+++ adi.py
@@ -32,15 +32,15 @@ def wishMe():
 
 def takeCommand():
 
-   r = sr.Recognizer()
+   result = sr.Recognizer()
    with sr.Microphone() as source:
       print("Listening...")
-      r.pause_threshold = 1
-      audio = r.listen(source)
+      result.pause_threshold = 1
+      audio = result.listen(source)
 
    try:
        print("Recognizing...")
-       query = r.recognize_google(audio, language = "en-in")
+       query = result.recognize_google(audio, language = "en-in")
        print("User said: ",query)
 
    except Exception as e:
